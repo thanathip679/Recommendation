1bc12b6436a1d6e7f9a38c9d35448474072c3382
thathanathip679 <thanathip.nan@gmail.com>
2026-02-17 17:10:19 +0700

    thanathipedit1

--- main.cpp/recommendation.h
+++ main.cpp/recommendation.h
@@ -1,7 +1,19 @@
 #ifndef RECOMMENDATION_H
 #define RECOMMENDATION_H
 
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<vector>
+#include "trip.h"
 
+using namespace std;
 
+//เช็ค tripstyle
+bool Check_tripstyle(Place &P, string ts);
+
+//แนะนำสถานที่
+vector<Place> Recommendation_place(vector<Place> &province, string TS);
+
+//โชว์สถานที่
+void Show_Place(vector<Place> &P);
 #endif
